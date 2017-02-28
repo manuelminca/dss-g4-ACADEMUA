aefde14338bb4debf4fceb3397f5c6dd4d29c461
MANUEL MÍNGUEZ CARRETERO <mmc148@clLS14I-22>
2017-02-28 10:39:25 +0100

    Created migrations

--- AcademUA/database/migrations/2017_02_28_091753_create_categories_table.php
+++ AcademUA/database/migrations/2017_02_28_091753_create_categories_table.php
@@ -14,7 +14,9 @@ class CreateCategoriesTable extends Migration
     public function up()
     {
         Schema::create('categories', function (Blueprint $table) {
-            $table->increments('id');
+  
+            $table->string('name');
+            $table->primary('name');
             $table->timestamps();
         });
     }
--- AcademUA/database/migrations/2017_02_28_091826_create_courses_table.php
+++ AcademUA/database/migrations/2017_02_28_091826_create_courses_table.php
@@ -15,7 +15,11 @@ class CreateCoursesTable extends Migration
     {
         Schema::create('courses', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('name');
+            $table->ftring('description');
+            $table->float('price');
             $table->timestamps();
+
         });
     }
 
