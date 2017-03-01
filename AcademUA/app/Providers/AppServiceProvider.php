a2b5c3ad9a8aa01f9a0e0954d04aa3f4aa21dd1a
Manuel Minguez <manuelinf12@gmai.com>
2017-03-01 18:30:54 +0100

    Models, migrations and seeders created

--- AcademUA/app/Providers/AppServiceProvider.php
+++ AcademUA/app/Providers/AppServiceProvider.php
@@ -6,23 +6,28 @@ use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Bootstrap any application services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        //
-    }
-
-    /**
-     * Register any application services.
-     *
-     * @return void
-     */
-    public function register()
-    {
-        //
-    }
+	
+	/**
+	* Bootstrap any application services.
+	     *
+	     * @return void
+	     */
+	    public function boot()
+	    {
+		if (config('database.default' ) == 'sqlite') {
+			$db = app()->make('db');
+			$db->connection ()->getPdo()->exec("PRAGMA foreign_keys = ON" );
+		}
+	}
+	
+	
+	/**
+	* Register any application services.
+	     *
+	     * @return void
+	     */
+	    public function register()
+	    {
+		//
+	}
 }
---
+++ AcademUA/database/seeds/UserscoursesTableSeeder.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserscoursesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+    }
+}
