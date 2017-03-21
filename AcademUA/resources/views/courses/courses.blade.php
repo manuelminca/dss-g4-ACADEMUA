877500ee1b75f3ad68ceef423bd18c8a70bb5b62
asehu <asehhu@gmail.com>
2017-03-21 10:59:32 +0100

    Modificado mostrar curso

--- AcademUA/resources/views/courses/courses.blade.php
+++ AcademUA/resources/views/courses/courses.blade.php
@@ -5,7 +5,11 @@
 <ul>
 <?php
 foreach ($courses as $course) {
-    echo "<li>" . $course->name . " - <a href='/courses/delete/" .$course->id. "'>Delete</a></li>";
+    echo "<li>";
+    echo $course->name . " Description: " . $course->description;
+    echo " - <a href='/courses/delete/" .$course->id. "'>Delete</a> - ";
+    echo "<a href='/courses/modify/" .$course->id. "'>Modify</a>";
+    echo "</li>";
 }
 
 //echo "<li>" . $user->name . " - <a href='/users/delete/" .$user->id. ">Delete</a></li>";
