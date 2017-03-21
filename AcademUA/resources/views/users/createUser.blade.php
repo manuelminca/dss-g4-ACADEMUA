ee81b669d157571acded36865a7d7505e2781865
manuelinf12@gmail.com <manuelinf12@gmail.com>
2017-03-21 14:47:42 +0100

    changed createUser

--- AcademUA/resources/views/users/createUser.blade.php
+++ AcademUA/resources/views/users/createUser.blade.php
@@ -2,15 +2,17 @@
 <body>
 @section('title','New User')
 <ul>
-<form action="/users/create/">
+<form action="{{action('UsersController@createUser')}}" method="POST">
+{{ csrf_field() }}
+{{ method_field('PUT') }}
   Name:<br>
   <input type="text" name="name"><br>
     Email:<br>
   <input type="text" name="email" <br><br>
       Password:<br>
-  <input type="text" name="pass" <br><br>
+  <input type="password" name="pass" <br><br>
       Repeat Password:<br>
-  <input type="text" name="pass2" <br><br>
+  <input type="password" name="pass2" <br><br>
       Username:<br>
   <input type="text" name="username" <br><br>
 
