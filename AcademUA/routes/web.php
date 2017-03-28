5942854ef0def6b2828b9c98eca38df859a02e3e
asehu <yeraipastor@gmail.com>
2017-03-28 13:02:34 +0200

    Changed web config

--- AcademUA/routes/web.php
+++ AcademUA/routes/web.php
@@ -83,7 +83,7 @@ Route::get('/modifiedCourse', function () {
 //Category
 Route::get('/categories/delete/{id}', 'CategoriesController@deleteCategory');
 
-Route::get('/categories'), 'CategoriesController@showCourses');
+Route::get('/categories', 'CategoriesController@showCourses');
 
 
 /*
