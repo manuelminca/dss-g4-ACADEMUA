89b2943d100e246fc5d64d50434401c31a544844
Manuel Minguez <manuelinf12@gmai.com>
2017-03-31 20:09:40 +0200

    Added Edit user, validations:
    -Edit User
    Validations of
    -Create course
    -Create User
    -Modify Course
    -Modify User

--- AcademUA/resources/views/users/modifyUser.blade.php
+++ AcademUA/resources/views/users/modifyUser.blade.php
@@ -1,15 +1,31 @@
-@extends('master')
+@include('master')
 <body>
+@section('title','Edit User')
 <ul>
+
+<h1>Modify User</h1>
+{{-- Error messages --}}
+@if (count($errors) > 0)
+<ul>
+@foreach ($errors->all() as $error)
+<li>{{ $error }}</li>
+@endforeach
+</ul>
+@endif
+
 <form action="/users/modified/{{$user->id}}">
+{{ csrf_field() }}
+{{ method_field('PUT') }}
+
   Name:<br>
-  <input type="text" name="name"><br>
+  <input value="{{ old('name') }}" type="text" name="name"><br>
     Email:<br>
-  <input type="text" name="email" <br><br>
+  <input value="{{ old('email') }}" type="text" name="email" <br><br>
       Password:<br>
-  <input type="text" name="pass" <br><br>
+  <input type="password" name="password" <br><br>
       Repeat Password:<br>
-  <input type="text" name="pass2" <br><br>
+  <input type="password" name="password_confirmation" <br><br>
+
   <input type="submit" value="Submit">
 </form>
 <?php
