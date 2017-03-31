f064a33c5b28497e3c6e1eb7f0c9d5f097b698c4
Manuel Minguez <manuelinf12@gmai.com>
2017-03-31 20:09:40 +0200

    Added Edit user, validations:
    -Edit User
    Validations of
    -Create course
    -Create User
    -Modify Course
    -Modify User

--- AcademUA/resources/views/courses/modifyCourse.blade.php
+++ AcademUA/resources/views/courses/modifyCourse.blade.php
@@ -3,14 +3,22 @@
 @section('title','Modify Course')
 
 <ul>
-
+<h1>Modify Course</h1>
+{{-- Error messages --}}
+@if (count($errors) > 0)
+<ul>
+@foreach ($errors->all() as $error)
+<li>{{ $error }}</li>
+@endforeach
+</ul>
+@endif
 
 
 <form action="/courses/modified/{{$courses->id}}">
   Name of the course:<br>
-  <input type="text" name="name"><br>
+  <input value="{{ old('name') }}" type="text" name="name"><br>
     Description:<br>
-  <input type="text" name="description" <br><br>
+  <input value="{{ old('description') }}" type="text" name="description" <br><br>
   <input type="submit" value="Submit">
 </form>
 
