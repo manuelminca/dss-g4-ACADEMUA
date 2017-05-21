dc0fc30deb460da32fbd87fba64af56365bb4339
Manuel Minguez <manuelinf12@gmai.com>
2017-05-21 23:47:27 +0200

    updated seeders

--- AcademUA/database/seeds/CategoriescoursesTableSeeder.php
+++ AcademUA/database/seeds/CategoriescoursesTableSeeder.php
@@ -14,17 +14,45 @@ class CategoriescoursesTableSeeder extends Seeder
 
         
         $pk1 = DB::table('categories')->where('name','programacion')->first();
-        $pk2 = DB::table('courses')->where('name', 'Curso completo')->first();
+        $pk2 = DB::table('courses')->where('name', 'Testing')->first();
         DB::table('category_course')->insert([
             'category_id' => $pk1->id,
             'course_id' => $pk2->id
         ]);
 
         $pk1 = DB::table('categories')->where('name','MultOS')->first();
-        $pk2 = DB::table('courses')->where('name', 'Java')->first();
+        $pk2 = DB::table('courses')->where('name', 'Java Course')->first();
         DB::table('category_course')->insert([
             'category_id' => $pk1->id,
             'course_id' => $pk2->id
         ]);
+
+        $pk1 = DB::table('categories')->where('name','Sports')->first();
+        $pk2 = DB::table('courses')->where('name', 'How to play Football')->first();
+        DB::table('category_course')->insert([
+            'category_id' => $pk1->id,
+            'course_id' => $pk2->id
+        ]);
+
+        $pk1 = DB::table('categories')->where('name','Politics')->first();
+        $pk2 = DB::table('courses')->where('name', 'Join a Political Party')->first();
+        DB::table('category_course')->insert([
+            'category_id' => $pk1->id,
+            'course_id' => $pk2->id
+        ]);
+
+        $pk1 = DB::table('categories')->where('name','MultOS')->first();
+        $pk2 = DB::table('courses')->where('name', 'php')->first();
+        DB::table('category_course')->insert([
+            'category_id' => $pk1->id,
+            'course_id' => $pk2->id
+        ]);
+        $pk1 = DB::table('categories')->where('name','Sports')->first();
+        $pk2 = DB::table('courses')->where('name', 'Play Tennis')->first();
+        DB::table('category_course')->insert([
+            'category_id' => $pk1->id,
+            'course_id' => $pk2->id
+        ]);
+        
     }
 }
