d471eca507b4986fdd493a82abfb8e1c5261c8e1
Manuel Minguez <manuelinf12@gmai.com>
2017-05-21 22:49:57 +0200

    fixed register with description

--- AcademUA/resources/views/auth/register.blade.php
+++ AcademUA/resources/views/auth/register.blade.php
@@ -69,6 +69,20 @@
                             </div>
                         </div>
 
+                        <div class="form-group{{ $errors->has('username') ? ' has-error' : '' }}">
+                            <label for="username" class="col-md-4 control-label">username</label>
+
+                            <div class="col-md-6">
+                                <input id="username" type="text" class="form-control" name="username" value="{{ old('username') }}" required>
+
+                                @if ($errors->has('username'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('username') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
                          <div class="form-group{{ $errors->has('professor') ? ' has-error' : '' }}">
                             <label for="professor" class="col-md-4 control-label">Are you a teacher?</label>
 
