06996a39e12d553b0a98dd9b65295e17a0a0d43b
quico14 <quicoll14@gmail.com>
2017-05-26 16:27:42 +0200

    Models commented

--- AcademUA/app/Message.php
+++ AcademUA/app/Message.php
@@ -19,22 +19,63 @@ class Message extends Model
 			        'id', 'subject', 'sender_id', 'receiver_id', 'message',
 			    ];
 
+	/*#############################################
+					Relationships
+	###############################################*/
+
+	//relation between receiver and messages
     public function user_receiver(){
         return $this->belongsTo('App\User', 'receiver_id');
     }
 
+	//relation between sender and messages
 	public function user_sender() {
 		return $this->belongsTo('App\User', 'sender_id');
 	}
 
-    public function deleteMessage(){
-		$this->delete();
-	}
+	/*#############################################
+					GETTERS AND SETTERS
+	###############################################*/
+
+	//get all messages
 	public function showMessages(){
 		$list = Message::all();
 		return $list;
 	}
 
+	//get messages that a user has received, given its id
+	public static function getReceivedMessages ($id) {
+		$user = Message::where('receiver_id','=',$id)->get();
+		return  $user;
+	}
+
+	//get messages that a user has sent, given its id
+	public static function getSentMessages ($id) {
+		$user = Message::where('sender_id','=',$id)->get();
+		return  $user;
+	}
+
+	//get the user who sent the actual message
+	public function getUserSend () {
+		$user = $this->user_sender->getUsername();
+		return  $user;
+	}
+
+	//get the user who received the actual message
+	public function getUserReceive () {
+		$name = $this->user_receiver->getUsername();
+		return  $name;
+	}
+
+	/*#############################################
+					Other functions
+	###############################################*/
+
+    public function deleteMessage(){
+		$this->delete();
+	}
+
+	//Create a message given the subject, the sender id, the receiver id and the message
 	public function createMessage ($subject, $sender_id, $receiver_id, $message) {
 		$this->subject = $subject;
 		$this->sender_id = $sender_id;
@@ -43,44 +84,21 @@ class Message extends Model
 		$this->save();
 	}
 
-	public static function getReceivedMessages ($id) {
-		$user = Message::where('receiver_id','=',$id)->get();
-		return  $user;
-	}
-
-	public static function getSentMessages ($id) {
-		$user = Message::where('sender_id','=',$id)->get();
-		return  $user;
-	}
-
-	public static function getSender () {
-		$user = Message::where('sender_id','=',$id)->get();
-		return  $user;
-	}
-
-	public function getUserSend () {
-		$user = $this->user_sender->getUsername();
-		return  $user;
-	}
-
-	public function getUserReceive () {
-		$name = $this->user_receiver->getUsername();
-		return  $name;
-	}
-
+	//Return received messages of the Authed user that has the request field in message or subject
 	public static function searchInput ($request, $id) {
-		$user = Message::where('receiver_id','=',$id)
+		$message = Message::where('receiver_id','=',$id)
 						 ->where(function($query) use ($request) {$query->where('subject', 'like', '%'. $request . '%')
 						 ->orWhere('message', 'like', '%'. $request . '%');
 						 })->get();
-		return $user;
+		return $message;
 	}
 
+	//Return sent messages of the Authed user that has the request field in message or subject
 	public static function searchOutput ($request, $id) {
-		$user = Message::where('sender_id','=',$id)
+		$message = Message::where('sender_id','=',$id)
 						 ->where(function($query) use ($request) {$query->where('subject', 'like', '%'. $request . '%')
 						 ->orWhere('message', 'like', '%'. $request . '%');
 						 })->get();
-		return $user;
+		return $message;
 	}
 }
