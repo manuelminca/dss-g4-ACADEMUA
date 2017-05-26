71e979dc468120380b64d908550473886f77eb83
quico14 <quicoll14@gmail.com>
2017-05-26 20:13:33 +0200

    Seeders messages

--- AcademUA/database/seeds/MessagesTableSeeder.php
+++ AcademUA/database/seeds/MessagesTableSeeder.php
@@ -17,14 +17,49 @@ class MessagesTableSeeder extends Seeder
             'subject' => 'Programación',
             'sender_id' => 1,
             'receiver_id' => 2,
-            'message' => 'hola'
+            'message' => 'Hey! Could you help me with an exercise?'
         ]);
 
         DB::table('messages')->insert([
             'subject' => 'Programación',
             'sender_id' =>2,
             'receiver_id' => 1,
-            'message' => 'adios'
+            'message' => 'Of course!'
+        ]);
+
+        DB::table('messages')->insert([
+            'subject' => 'Programación',
+            'sender_id' => 1,
+            'receiver_id' => 2,
+            'message' => 'Hey! Could you help me with an exercise?'
+        ]);
+
+        DB::table('messages')->insert([
+            'subject' => 'Programación',
+            'sender_id' =>3,
+            'receiver_id' => 4,
+            'message' => 'Are you interested in my course about programming?'
+        ]);
+
+        DB::table('messages')->insert([
+            'subject' => 'Programación',
+            'sender_id' => 4,
+            'receiver_id' => 3,
+            'message' => 'Yes, I am going to attend'
+        ]);
+
+        DB::table('messages')->insert([
+            'subject' => 'Programación',
+            'sender_id' =>5,
+            'receiver_id' => 6,
+            'message' => 'Thanks for your course it has helped me a lot!'
+        ]);
+        
+        DB::table('messages')->insert([
+            'subject' => 'Programación',
+            'sender_id' =>6,
+            'receiver_id' => 5,
+            'message' => 'You are welcome, nice to help you.'
         ]);
     }
 }
