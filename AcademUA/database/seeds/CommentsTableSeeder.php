dfc73400263c1abc7ce3104e044da434850ae401
asehu <asehhu@gmail.com>
2017-05-26 20:11:35 +0200

    Modificados los seeders

--- AcademUA/database/seeds/CommentsTableSeeder.php
+++ AcademUA/database/seeds/CommentsTableSeeder.php
@@ -13,10 +13,133 @@ class CommentsTableSeeder extends Seeder
     {
 
         DB::table('comments')->insert([
-		            'description' => 'Curso de iniciación a Java',
-                    'rating' => 1,
-                    'user_id' => 1,
-                    'course_id' => 4
-		        ]);
+            'description' => 'Curso de iniciación a Java',
+            'rating' => 1,
+            'user_id' => 1,
+            'course_id' => 4
+        ]);
+
+        DB::table('comments')->insert([
+            'description' => 'Best course!! Really interesting.',
+            'rating' => 4,
+            'user_id' => 2,
+            'course_id' => 1
+        ]);
+        DB::table('comments')->insert([
+            'description' => 'Good course and good teacher.',
+            'rating' => 3,
+            'user_id' => 1,
+            'course_id' => 1
+        ]);
+        DB::table('comments')->insert([
+            'description' => 'I have enjoyed and learned a lot.',
+            'rating' => 5,
+            'user_id' => 5,
+            'course_id' => 1
+        ]);
+        DB::table('comments')->insert([
+            'description' => 'Can you answer my message? I have some troubles with the lesson 3.',
+            'rating' => 5,
+            'user_id' => 5,
+            'course_id' => 1
+        ]);
+       
+        DB::table('comments')->insert([
+            'description' => 'Best course!! Really interesting.',
+            'rating' => 4,
+            'user_id' => 2,
+            'course_id' => 2
+        ]);
+        DB::table('comments')->insert([
+            'description' => 'Good course and good teacher.',
+            'rating' => 3,
+            'user_id' => 1,
+            'course_id' => 2
+        ]);
+        DB::table('comments')->insert([
+            'description' => 'I have enjoyed and learned a lot.',
+            'rating' => 1,
+            'user_id' => 5,
+            'course_id' => 2
+        ]);
+        DB::table('comments')->insert([
+            'description' => 'Can you answer my message? I have some troubles with the lesson 3.',
+            'rating' => 2,
+            'user_id' => 5,
+            'course_id' => 2
+        ]);
+
+         DB::table('comments')->insert([
+            'description' => 'Best course!! Really interesting.',
+            'rating' => 4,
+            'user_id' => 2,
+            'course_id' => 3
+        ]);
+        DB::table('comments')->insert([
+            'description' => 'Good course and good teacher.',
+            'rating' => 1,
+            'user_id' => 1,
+            'course_id' => 3
+        ]);
+        DB::table('comments')->insert([
+            'description' => 'I have enjoyed and learned a lot.',
+            'rating' => 3,
+            'user_id' => 5,
+            'course_id' => 3
+        ]);
+        DB::table('comments')->insert([
+            'description' => 'Can you answer my message? I have some troubles with the lesson 3.',
+            'rating' => 4,
+            'user_id' => 5,
+            'course_id' => 3
+        ]);
+         DB::table('comments')->insert([
+            'description' => 'Best course!! Really interesting.',
+            'rating' => 4,
+            'user_id' => 2,
+            'course_id' => 5
+        ]);
+        DB::table('comments')->insert([
+            'description' => 'Good course and good teacher.',
+            'rating' => 5,
+            'user_id' => 1,
+            'course_id' => 5
+        ]);
+        DB::table('comments')->insert([
+            'description' => 'I have enjoyed and learned a lot.',
+            'rating' => 5,
+            'user_id' => 5,
+            'course_id' => 5
+        ]);
+        DB::table('comments')->insert([
+            'description' => 'Can you answer my message? I have some troubles with the lesson 3.',
+            'rating' => 5,
+            'user_id' => 5,
+            'course_id' => 5
+        ]);
+         DB::table('comments')->insert([
+            'description' => 'Best course!! Really interesting.',
+            'rating' => 4,
+            'user_id' => 2,
+            'course_id' => 6
+        ]);
+        DB::table('comments')->insert([
+            'description' => 'Good course and good teacher.',
+            'rating' => 3,
+            'user_id' => 1,
+            'course_id' => 7
+        ]);
+        DB::table('comments')->insert([
+            'description' => 'I have enjoyed and learned a lot.',
+            'rating' => 5,
+            'user_id' => 5,
+            'course_id' => 8
+        ]);
+        DB::table('comments')->insert([
+            'description' => 'Can you answer my message? I have some troubles with the lesson 3.',
+            'rating' => 5,
+            'user_id' => 5,
+            'course_id' => 8
+        ]);
     }
 }
--- AcademUA/database/seeds/SessionsTableSeeder.php
+++ AcademUA/database/seeds/SessionsTableSeeder.php
@@ -46,7 +46,123 @@ class SessionsTableSeeder extends Seeder
             'course_id' => '1'
 
         ]);
-    
 
+        //Course 2
+         DB::table('sessions')->insert([
+            'title' => 'Session 1',
+            'content' => 'This is the first video of the Course, enjoy!',
+            'video' => 'https://www.youtube.com/embed/uxIF8upjjRA',
+            'course_id' => '2'
+        ]);
+         DB::table('sessions')->insert([
+            'title' => 'Session 2',
+            'content' => 'Video',
+            'video' => 'https://www.youtube.com/embed/R5H72_sGB2g',
+            'course_id' => '2'
+        ]);
+         DB::table('sessions')->insert([
+            'title' => 'Session 3',
+            'content' => 'Video',
+            'video' => 'https://www.youtube.com/embed/fEJcIV6vngI',
+            'course_id' => '2'
+        ]);
+
+         //Course 3
+         DB::table('sessions')->insert([
+            'title' => 'Session 1',
+            'content' => 'This is the first video of the Course, enjoy!',
+            'video' => 'https://www.youtube.com/embed/iCUV3iv9xOs',
+            'course_id' => '3'
+        ]);
+         DB::table('sessions')->insert([
+            'title' => 'Session 2',
+            'content' => 'Video',
+            'video' => 'https://www.youtube.com/embed/k6ZiPqsBvEQ',
+            'course_id' => '3'
+        ]);
+
+         //Course 4
+         DB::table('sessions')->insert([
+            'title' => 'Session 1',
+            'content' => 'This is the first video of the Course, enjoy!',
+            'video' => 'https://www.youtube.com/embed/wTqZK7EwD08',
+            'course_id' => '4'
+        ]);
+         DB::table('sessions')->insert([
+            'title' => 'Session 2',
+            'content' => 'Video',
+            'video' => 'https://www.youtube.com/embed/RU-2rFHuXf0',
+            'course_id' => '4'
+        ]);
+         DB::table('sessions')->insert([
+            'title' => 'Session 3',
+            'content' => 'Video',
+            'video' => 'https://www.youtube.com/embed/gX36pbl7GJU',
+            'course_id' => '4'
+        ]);
+
+        //Course 5
+         DB::table('sessions')->insert([
+            'title' => 'Session 1',
+            'content' => 'This is the first video of the Course, enjoy!',
+            'video' => 'https://www.youtube.com/embed/wTqZK7EwD08',
+            'course_id' => '5'
+        ]);
+         DB::table('sessions')->insert([
+            'title' => 'Session 2',
+            'content' => 'Video',
+            'video' => 'https://www.youtube.com/embed/-1yfWb0-jqQ',
+            'course_id' => '5'
+        ]);
+
+         //Course 6
+         DB::table('sessions')->insert([
+            'title' => 'Session 1',
+            'content' => 'This is the first video of the Course, enjoy!',
+            'video' => 'https://www.youtube.com/embed/I8XXfgF9GSc',
+            'course_id' => '6'
+        ]);
+         DB::table('sessions')->insert([
+            'title' => 'Session 2',
+            'content' => 'Video',
+            'video' => 'https://www.youtube.com/embed/M_6z8L8qK8o',
+            'course_id' => '6'
+        ]);
+         DB::table('sessions')->insert([
+            'title' => 'Session 3',
+            'content' => 'Video',
+            'video' => 'https://www.youtube.com/embed/2Ekty7t621k',
+            'course_id' => '6'
+        ]);
+        
+        //Course 5
+         DB::table('sessions')->insert([
+            'title' => 'Session 1',
+            'content' => 'This is the first video of the Course, enjoy!',
+            'video' => 'https://www.youtube.com/embed/wTqZK7EwD08',
+            'course_id' => '5'
+        ]);
+         DB::table('sessions')->insert([
+            'title' => 'Session 2',
+            'content' => 'Video',
+            'video' => 'https://www.youtube.com/embed/-1yfWb0-jqQ',
+            'course_id' => '5'
+        ]);
+
+         //Course 6
+         DB::table('sessions')->insert([
+            'title' => 'Session 1',
+            'content' => 'Learn to drive a car!',
+            'video' => 'https://www.youtube.com/embed/Zfe4wX5bEys',
+            'course_id' => '7'
+        ]);
+
+         //Course 8
+         DB::table('sessions')->insert([
+            'title' => 'Session 1',
+            'content' => 'Learn to drive a car!',
+            'video' => 'https://www.youtube.com/embed/juKd26qkNAw',
+            'course_id' => '8'
+        ]);
     }
 }
